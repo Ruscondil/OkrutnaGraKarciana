ab17dc67995f4e83995eab541569ecbfa6c4bc6a
Ruscondil <kacpers2001@o2.pl>
2023-01-14 12:23:39 +0100

    stop timer

--- server/timer.cpp
+++ server/timer.cpp
@@ -12,19 +12,26 @@ int Timer::getTimeLeft() const
 
 void Timer::startTimer(int seconds)
 {
+
     if (_timerThread.joinable())
     {
         error(0, 0, "Timer jest już odpalony");
         return;
     }
+    _stop = false;
     _seconds = seconds;
     _timerThread = std::thread(&Timer::countdown, this);
+}
+
+void Timer::stopTimer()
+{
+    _stop = true;
 }
 
 void Timer::countdown()
 {
     auto start = std::chrono::system_clock::now();
-    while (_seconds > 0)
+    while (_seconds > 0 && !_stop)
     {
         auto now = std::chrono::system_clock::now();
         std::chrono::duration<double> elapsed = now - start;
@@ -35,7 +42,8 @@ void Timer::countdown()
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-    timerDone();
+    if (!_stop)
+        timerDone();
 }
 
 void Timer::timerDone()
--- server/timer.hpp
+++ server/timer.hpp
@@ -5,12 +5,14 @@ class Timer
 {
     void countdown();
     int _seconds;
+    bool _stop;
     std::thread _timerThread;
 
 public:
     Timer();
     int getTimeLeft() const;
     void startTimer(int seconds);
+    void stopTimer();
     virtual void timerDone();
     virtual void secondPassed();
     ~Timer();
